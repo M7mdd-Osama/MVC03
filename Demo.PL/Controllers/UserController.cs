d8854184652cbee088b80bf00ceab6239c6d7cbb
Mohamed Osama Ali <ana20332183@gmail.com>
2024-10-17 22:04:26 +0300

    User Controller - Index

---
+++ Demo.PL/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using Demo.DAL.Models;
+using Demo.PL.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.PL.Controllers
+{
+	public class UserController : Controller
+	{
+		private readonly UserManager<ApplicationUser> _userManager;
+
+		public UserController(UserManager<ApplicationUser> userManager)
+		{
+			_userManager = userManager;
+		}
+		public async Task<IActionResult> Index(string SearchValue)
+		{
+			if (string.IsNullOrEmpty(SearchValue))
+			{
+				var Users = await _userManager.Users.Select(
+					U => new UserViewModel()
+					{
+						Id = U.Id,
+						FName = U.Fname,
+						LName = U.Lname,
+						Email = U.Email,
+						PhoneNumber = U.PhoneNumber,
+						Roles = _userManager.GetRolesAsync(U).Result
+					}).ToListAsync();
+				return View(Users);
+			}
+			else
+			{
+				var User = await _userManager.FindByEmailAsync(SearchValue);
+				var MappedUser = new UserViewModel()
+				{
+					Id = User.Id,
+					FName = User.Fname,
+					LName = User.Lname,
+					Email = User.Email,
+					PhoneNumber = User.PhoneNumber,
+					Roles = _userManager.GetRolesAsync(User).Result
+				};
+				return View(new List<UserViewModel> { MappedUser });
+			}
+		}
+	}
+}
